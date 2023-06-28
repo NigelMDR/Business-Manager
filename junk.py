e6281a222c89da68aed483d213552ab70653314a
nigel <ndamian4@unm.edu>
2023-06-28 17:05:00 -0600

    update graph 1 aka p1 and added threshold stats

--- junk.py
+++ junk.py
@@ -1,6 +1,15 @@
 import numpy as np
-arr = np.random.randn(20, 3)
-arr = np.array()
+import pandas as pd
+
+
+table_xy = {}
+arr_x = [1,2,3]
+arr_y = [1,2,3]
+table_xy['gross income'] = {'x': arr_x, 'y': arr_y}
+print(table_xy['gross income'].i)
+
+
+arr = [1,2]
 print(arr)
 
 def step_function(const: None, step, max):
