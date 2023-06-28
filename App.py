211d03d04d50b3221f977e8635fac2d1545c177d
nigel <ndamian4@unm.edu>
2023-06-28 01:32:07 -0600

    second chart

--- App.py
+++ App.py
@@ -60,6 +60,7 @@ st.write('_______')
 
 col1n, col2n, col3n = st.columns(3)
 col1n.metric(':bar_chart: Costo Operativo',str(stats['Operating Cost']['#']) + 'S', str(stats['Operating Cost']['%']) + '%', delta_color='off')
+
 col2n.metric(':bar_chart: Costo del Empleado',str(stats['Employee Cost']['#']) + 'S', str(stats['Employee Cost']['%']) + '%', delta_color='off')
 col3n.metric(':bar_chart: Deuda Bancaria',str(stats['Bank Debt']['#']) + 'S', str(stats['Bank Debt']['%']) + '%', delta_color='off')
 
@@ -67,75 +68,109 @@ col3n.metric(':bar_chart: Deuda Bancaria',str(stats['Bank Debt']['#']) + 'S', st
 #                                   Graphing                                   #
 # ---------------------------------------------------------------------------- #
 
-# fx = lambda x: ((x*(1-tax))*(1-stats['Operating Cost']['%']/100))*(1-stats['Employee Cost']['%']/100)
-fx = lambda x: x*stats['gross profit']['%']/100
-fx2 = lambda x: x*.27
-fx3 = lambda x: x+1
+def get_data():
+  
+  M = 25000
+  
+  x1 = [x for x in range(1,M)]
+  fx1 = lambda x: x*stats['gross profit']['%']/100
+  y1 = [fx1(x) for x in x1]
+  
+  y2 = [x for x in range(1,M)]
+  x2 = [stats['revenue']['#'] for x in y2]
+  
+  x3 = [x for x in range(1,M)]
+  y3 = [stats['gross profit']['#'] for x in range(1,M)]
 
-x1 = [fx3(x) for x in range(-1,25000)]
-y1 = []
-i = 0
-for x in x1:
-  y1.append(fx(x))
+  x4 = [x for x in range(1,M)]
+  fx4 = lambda x: x*(1-tax) - cost - stats['Employees Salary']
+  y4 = [fx4(x) for x in x4]
 
-bounds = 0
-temp = y1[::-1]
-for val in temp:
-  if val <= stats['gross profit']['#']:
-    bounds = x1[y1.index(val)]
-    break;
+  y5 = [x for x in range(1,M)]
+  x5 = [income for x in y2]
+  
+  x6 = [x for x in range(1,M)]
+  y6 = [fx4(income) for x in y2]
+  
+  val = 0
+  y7 = [x for x in range(1,M)]
+  for idx, y in enumerate(y4):
+    if y >= 0:
+      val = idx
+      break;
     
-# Verticle Line
+  x7 = [val for x in y7]
 
-x2 = [stats['revenue']['#'] for val in range(-1,25000)]
-y2 = [fx3(x) for x in range(-1,25000)]
 
-x5 = [bounds for val in range(-1,25000)]
-y5 = [fx3(x) for x in range(-1,25000)]
+  return x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7
 
-# Horizontal line
+x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7 = get_data()
 
-x4 = [fx3(x) for x in range(-1,25000)]
-y4 = [stats['gross profit']['#'] for x in range(-1,25000)]
-
-p = figure(
-    title='Informe de meses basados en proyección',
+p1 = figure(
+    title='Margen de Ingresos Brutos',
     x_axis_label='Ganancia',
     y_axis_label='Beneficio Bruto',
     sizing_mode="stretch_width",
     tooltips="(@x, @y)",
-    x_range=(0, income+1000),
-    y_range=(0, fx(income)),
     height=400,
     )
 
+p1.toolbar_location = "below"
+p1.xaxis.ticker = [0, income, stats['revenue']['#']]
+p1.yaxis.ticker = [0, stats['gross profit']['#'] ]
+p1.line(x1, y1, line_width=2, color= 'navy', legend_label='Margen de ingresos brutos')
+p1.line(x2, y2, line_width=2, color= 'red', legend_label='Ganancia', line_dash='dashed')
+p1.line(x3, y3, line_width=2, color= 'green', legend_label='Beneficio Bruto', line_dash='dashed')
+
+p2 = figure(
+    title='Ingresos Brutos',
+    x_axis_label='Ingreso',
+    y_axis_label='Beneficio Bruto',
+    sizing_mode="stretch_width",
+    tooltips="(@x, @y)",
+    height=400,
+    )
+
+p2.toolbar_location = "below"
+p2.xaxis.ticker = [0, income, x7[0]]
+p2.yaxis.ticker = [0, stats['gross profit']['#'] ]
+p2.line(x4, y4, line_width=2, color= 'orange', legend_label='Ingresos brutos')
+p2.line(x5, y5, line_width=2, color= 'blue', legend_label='ingreso', line_dash='dashed')
+p2.line(x6, y6, line_width=2, color= 'black', legend_label='n/a', line_dash='dashed')
+p2.line(x7, y7, line_width=2, color= 'red', legend_label='Peligro', line_dash='dashed')
+
 low_box = BoxAnnotation(top=monthly_debt, fill_alpha=0.2, fill_color='red')
-p.add_layout(low_box)
+p1.add_layout(low_box)
+low_box2 = BoxAnnotation(top=monthly_debt, fill_alpha=0.2, fill_color='red')
+p2.add_layout(low_box2)
 
-p.line(x1, y1, line_width=2, color= 'navy', legend_label='Proyección')
-p.line(x2, y2, line_width=2, color= 'red', legend_label='Ganancia', line_dash='dashed')
-p.line(x4, y4, line_width=2, color= 'green', legend_label='Beneficio Bruto', line_dash='dashed')
-
-
-p.xaxis.ticker = [0, income, stats['revenue']['#']]
-p.yaxis.ticker = [0, stats['gross profit']['#'] ]
-p.toolbar_location = "below"
-
-st.bokeh_chart(p)
+st.bokeh_chart(p1)
+st.bokeh_chart(p2)
 
 # ---------------------------------------------------------------------------- #
 #                                     Notes                                    #
 # ---------------------------------------------------------------------------- #
 
 code = '''
+
+     Impuesto % : Porcentaje tomado por la empresa
+     Pago de Empleados % : Porcentaje de cada empleado que se llevará a casa 'salario'
+
      impuestos = 0.27
-     Ganancia = ingreso*(1-impuesto)
-     Costo operativo = (Ganancia - Costo) / Ganancia 
-     Costo del empleado = (Ganancia - salario total del empleado) / Ganancia 
-     Proyección(x) = x*(1 - impuesto)*(1 - costo operativo)*(1 - Costo del empleado)
-     Beneficio Bruto = Ganancia - Costo - salario total del empleado
-     Margen de beneficio bruto = Beneficio Bruto / Ganancia
-     Ingresos Netos = Beneficio Bruto - Deuda bancaria mensual
+     # Ganancia = ingreso*(1-impuestos)
+     % Costo operativo = 1 - (Ganancia - (Los Gastos Operativos)) / Ganancia * 100
+     # salario total del empleado = (Pago de Empleados)/100 * (Empleado 1 +  Empleado 2 + ... + Empleado 5)
+     % Costo del empleado = 1 - (Ganancia - (salario total del empleado)) / Ganancia * 100
+     # Beneficio Bruto = Ganancia - (Los Gastos Operativos) - (salario total del empleado)
+     % Margen de beneficio bruto = 1 - (Beneficio Bruto) / Ganancia * 100
+     # Ingresos Netos = (Beneficio Bruto) - (Deuda bancaria mensual)
+     
+     x es la variable
+     Margen de ingresos brutos = x * (Margen de beneficio bruto)
+     ingresos brutos = x*(1-tax) - (Los Gastos Operativos) - (salario total del empleado)
+     
     '''
 st.code(code, language='ltex')
+
+
 
